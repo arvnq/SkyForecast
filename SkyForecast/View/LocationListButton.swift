c726017081b52ade14d28572734bf7ea7d7c07fd
arvnq <arvin.quiliza@gmail.com>
2018-12-10 16:26:18 +1100

    changes width and height constraint of location list button

--- SkyForecast/View/LocationListButton.swift
+++ SkyForecast/View/LocationListButton.swift
@@ -29,7 +29,7 @@ class LocationListButton: UIButton {
         self.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             self.heightAnchor.constraint(equalToConstant: 20.0),
-            self.widthAnchor.constraint(equalToConstant: 30.0)
+            self.widthAnchor.constraint(equalToConstant: 40.0)
         ])
         self.setImage(UIImage(named: "location.pdf"), for: .normal)
         self.imageView?.tintColor = .black
