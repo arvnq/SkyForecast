4ec7cdbc0a50dc7a9e5b34d8f7ebdeb48a5f8382
arvnq <arvin.quiliza@gmail.com>
2018-12-10 16:29:11 +1100

    added method for setting up attributed text for labels in forecast

--- SkyForecast/Helper/Extension/NSAttributedString+Helper.swift
+++ SkyForecast/Helper/Extension/NSAttributedString+Helper.swift
@@ -7,11 +7,29 @@
 //
 
 import Foundation
+import UIKit
 
 extension NSAttributedString {
-    static func setupAttributedString(for field: String, using value: Strong) -> NSAttributedString {
-        let regularAttribute: [NSAttributedString.key: Any] = [
-            .font: UIFont.systemFont(ofSize: 14.0)
+    static func setupText(forField field: String, usingValue value: String) -> NSAttributedString {
+
+//        no use for these attributes at the moment
+//        saving for future versions
+//        let regularFontAttribute: [ NSAttributedString.Key: Any] = [
+//            .font: UIFont.systemFont(ofSize: 14.0)
+//        ]
+//
+//        let boldFontAttribute: [ NSAttributedString.Key: Any ] = [
+//            .font: UIFont.boldSystemFont(ofSize: 14.0)
+//        ]
+        
+        let fgColorAttribute: [ NSAttributedString.Key: Any ] = [
+            .foregroundColor: UIColor.gray,
         ]
+        
+        
+        let attributedFieldValue = NSMutableAttributedString(string: field, attributes: fgColorAttribute)
+        attributedFieldValue.append(NSAttributedString(string: value, attributes: [:]))
+        
+        return attributedFieldValue
     }
 }
